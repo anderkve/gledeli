987295f67c5c12e55beeb0b35c0cdd068a4e0428
fzeiser <fzeiser@physnet.uni-hamburg.de>
2021-01-13 11:39:17 +0100

    Auto-recognize model in results plotting

--- plotting/posterior_draw_samples.py
+++ plotting/posterior_draw_samples.py
@@ -36,14 +36,24 @@ class HDFLoader:
 
     Attributes:
         nld_model: name of nld model (run `load` or `get_nld_model` to set it)
+        gsf_model: name of gsf model (run `load` or `get_gsf_model` to set it)
     """
 
     def __init__(self):
         # list of model names [GAMBIT, gledeli]
-        self._model_names = \
+        self._nld_model_names = \
             [["NLDModelBSFG_and_discretes", "bsfg_and_discrete"],
              ["NLDModelCT_and_discretes", "ct_and_discrete"]]
+        self._gsf_model_names = \
+            [["GSFModel20", "GLO-CT"],
+             ["GSF_GLO_CT_Model20", "GLO-CT"],
+             ["GSF_EGLO_CT_Model20", "EGLO-CT"],
+             ["GSF_MGLO_CT_Model20", "MGLO-CT"],
+             ["GSF_GH_CT_Model20", "GH-CT"],
+             #["GSF_constantM1","SP"]
+             ]
         self.nld_model: Optional[str] = None
+        self.gsf_model: Optional[str] = None
 
     def load(self, fname: Union[str, Path]) -> Tuple[pd.DataFrame, Dict]:
         """ loads results from hdf5 file as pd.DataFrame
@@ -59,10 +69,11 @@ class HDFLoader:
         file = h5py.File(fname, 'r')
         group = file['/scan_output/']
 
-        nld_model = self.get_nld_model(group)
+        nld_model = self.get_models(group, "nld")
+        gsf_model = self.get_models(group, "gsf")
 
         data = {}
-        data["gsf"] = self.hdf5_gsf_pars(group)
+        data["gsf"] = self.hdf5_gsf_pars(group, gsf_model)
         data["nld"] = self.hdf5_nld_pars(group, nld_model)
         data["other"] = self.hdf5_other_pars(group)
         data["results"] = self.hdf5_results_pars(group)
@@ -87,26 +98,67 @@ class HDFLoader:
 
         return data, names
 
-    def get_nld_model(self, group) -> dict:
+    def get_models(self, group: h5py._hl.group.Group,
+                   which: str ="nld") -> dict:
+        if which == "nld":
+            known_models = self._nld_model_names
+        elif which == "gsf":
+            known_models = self._gsf_model_names
+        else:
+            raise NotImplementedError('Can only find models in ["gsf", "nld"]',
+                                      f'but which is set to {which}')
+
+        model = self._get_model(group, known_models)
+
+        if which == "nld":
+            self.nld_model = model["gledeli"]
+        elif which == "gsf":
+            self.gsf_model = model["gledeli"]
+        return model
+
+    @staticmethod
+    def _get_model(group, known_models) -> dict:
         keys = group.keys()
-        for pair in self._model_names:
+        for pair in known_models:
             if any(re.match(".*"+pair[0]+".*", key) for key in keys):
                 names = pair
                 break
         try:
             model = {"gambit": names[0], "gledeli": names[1]}
         except NameError:
-            raise NotImplementedError("No known nld model")
-        self.nld_model = model["gledeli"]
+            raise NotImplementedError(f"No known model name in {keys}")
         return model
 
     @staticmethod
-    def hdf5_gsf_pars(group: h5py._hl.group.Group) -> dict:
+    def hdf5_gsf_pars(group: h5py._hl.group.Group, model_name: dict) -> dict:
         gsf_data = {}
-        basename = '#GSFModel20_parameters @GSFModel20::primary_parameters::' \
-                   'gsf_p{i}'
-        for i in range(1, 21):
-            gsf_data[f'p{i}'] = np.array(group[basename.format(i=i)])
+        basename = f'#{model_name["gambit"]}_parameters '\
+                   f'@{model_name["gambit"]}''::primary_parameters::gsf_{key}'
+        for i in range(1, 20):
+            gambit_name = basename.format(key=f"p{i}")
+            gsf_data[f'gsf_p{i}'] = np.array(group[gambit_name])
+
+        if model_name["gledeli"] in ["MGLO-CT", "EGLO-CT"]:
+            p_extra = ["T", "epsilon_0", "k"]
+        elif model_name["gledeli"] == ["GH-CT"]:
+            p_extra = ["T", "k"]
+        elif model_name["gledeli"] == ["GLO-CT"]:
+            p_extra = ["T"]
+
+        for par in p_extra:
+            gsf_data[f"gsf_{par}"] = np.array(group[basename.format(key=par)])
+
+        # TODO: clean up after gledeli/gambit_np refactoring
+        try:
+            model_name = ["GSF_constantM1", "constantM1"]
+            basename = f'#{model_name[0]}_parameters '\
+                       f'@{model_name[0]}''::primary_parameters::gsf_{key}'
+            par = model_name[1]
+            gsf_data[f"gsf_{par}"] = np.array(group[basename.format(key=par)])
+        except KeyError:
+            print("Log: Didn't find constantM1 amonst keys")
+            pass
+
         return gsf_data
 
     @staticmethod
@@ -121,7 +173,7 @@ class HDFLoader:
         else:
             raise NotImplementedError()
         for key in keys:
-            nld_data[f'{key}'] = np.array(group[basename.format(key=key)])
+            nld_data[f'nld_{key}'] = np.array(group[basename.format(key=key)])
         return nld_data
 
     @staticmethod
@@ -159,13 +211,26 @@ class PosteriorPlotter:
         self._nld = self.glede._nld
         self._gsf = self.glede._gsf
 
-    def gsf_plot(self,
-                 df: pd.DataFrame,
-                 sort_by: Optional[str] = "posterior_weights",
-                 sort_ascending: bool = False,
-                 weights: Optional[str] = None,
-                 n_samples: int = 100,
-                 x: Optional[np.array] = None, ax=None):
+    def gsf_plot(self, *args, **kwargs):
+        """ wrapper for _gsf_plot """
+        if len(args) > 0:
+            df = args[0]
+            args = list(args)
+            renamed = rename_gsf_columns(df)
+            args[0] = renamed
+        elif "df" in kwargs.keys():
+            df = kwargs["df"]
+            renamed = rename_gsf_columns(df)
+            kwargs["df"] = renamed
+        return self._gsf_plot(*args, **kwargs)
+
+    def _gsf_plot(self,
+                  df: pd.DataFrame,
+                  sort_by: Optional[str] = "posterior_weights",
+                  sort_ascending: bool = False,
+                  weights: Optional[str] = None,
+                  n_samples: int = 100,
+                  x: Optional[np.array] = None, ax=None):
         """ Plots gsf samples
 
         Args:
@@ -206,11 +271,12 @@ class PosteriorPlotter:
                     assert alpha > -0.01
                     alpha = 0
 
-            ax.plot(x, CreateGSF.model(x, dic), '-',
+            creategsf = CreateGSF(pars=dic)
+            ax.plot(x, creategsf.create(x).values, '-',
                     color='k', alpha=alpha)
-            ax.plot(x, CreateGSF.model_E1(x, dic), '--',
+            ax.plot(x, creategsf.create(x, kind="E1").values, '--',
                     color='b', alpha=alpha)
-            ax.plot(x, CreateGSF.model_M1(x, dic), '--',
+            ax.plot(x, creategsf.create(x, kind="M1").values, '--',
                     color='g', alpha=alpha)
             if i == n_samples:
                 break
@@ -224,6 +290,13 @@ class PosteriorPlotter:
         ax.plot([], [], '-', color='k', label='samples, sum')
         ax.plot([], [], '--', color='b', label='samples, E1')
         ax.plot([], [], '--', color='g', label='samples, M1')
+
+        try:
+            self.plot_analyzed_om_result(ax,
+                gledelig_path/"data/162Dy_oslo/old_analysis/gsf.txt") # noqa
+        except OSError:
+            print("No old Oslo data available")
+
         ax.legend()
 
         ax.set_yscale('log')
@@ -231,15 +304,43 @@ class PosteriorPlotter:
         ax.set_ylabel(rf"$\gamma$-SF f($E_\gamma$) [MeV$^{{-3}}$]")
         return fig, ax
 
-    def nld_plot(self,
-                 df: pd.DataFrame,
-                 data_path: Union[str, Path],
-                 sort_by: Optional[str] = "posterior_weights",
-                 sort_ascending: bool = False,
-                 weights: Optional[str] = None,
-                 n_samples: int = 100,
-                 x: Optional[np.array] = None,
-                 ax=None):
+    @staticmethod
+    def plot_analyzed_om_result(ax, file, **kwargs):
+        """ Plot analyzed oslo method results on top
+
+        Assumed format: [E, values, err, err_tot]
+            where we will use the columns with err_tot, including
+            normalization error
+         """
+        data = np.loadtxt(file, usecols=[0, 1, 3])
+        kwargs.setdefault("alpha", 0.2)
+        kwargs.setdefault("fmt", "<")
+        kwargs.setdefault("label", "OM analysis")
+        ax.errorbar(data[:, 0], data[:, 1], data[:, 2], **kwargs)
+        ax.legend()
+
+    def nld_plot(self, *args, **kwargs):
+        """ wrapper for _nld_plot """
+        if len(args) > 0:
+            df = args[0]
+            args = list(args)
+            renamed = rename_nld_columns(df)
+            args[0] = renamed
+        elif "df" in kwargs.keys():
+            df = kwargs["df"]
+            renamed = rename_nld_columns(df)
+            kwargs["df"] = renamed
+        return self._nld_plot(*args, **kwargs)
+
+    def _nld_plot(self,
+                  df: pd.DataFrame,
+                  data_path: Union[str, Path],
+                  sort_by: Optional[str] = "posterior_weights",
+                  sort_ascending: bool = False,
+                  weights: Optional[str] = None,
+                  n_samples: int = 100,
+                  x: Optional[np.array] = None,
+                  ax=None):
         """ Plots nld samples
 
         Note:
@@ -303,6 +404,12 @@ class PosteriorPlotter:
         ax.plot([], [], '-', color='b', label='samples')
         ax.legend()
 
+        try:
+            self.plot_analyzed_om_result(ax,
+                gledelig_path/"data/162Dy_oslo/old_analysis/nld.txt") # noqa
+        except OSError:
+            print("No old Oslo data available")
+
         ax.set_yscale('log')
         ax.set_ylabel(r"Level density $\rho(E_x)~[\mathrm{MeV}^{-1}]$")
         ax.set_xlabel(r"Excitation energy $E_x~[\mathrm{MeV}]$")
@@ -311,7 +418,7 @@ class PosteriorPlotter:
         return fig, ax
 
     def compare_firstgen(self,
-                         pars: pd.DataFrame, ax=None):
+                         pars: pd.Series, ax=None):
         """ Compare input first generation spectrum to a fit
 
         Args:
@@ -324,14 +431,18 @@ class PosteriorPlotter:
         """
         if ax is None:
             nexp = len(glede._lnlikefgs)
+            nexp=2
             fig, ax = plt.subplots(2, nexp, constrained_layout=True)
             fig.set_size_inches(4*nexp, 6)
         else:
             fig, ax = ax.figure, ax
 
         nld = copy.deepcopy(self._nld)
-        nld.pars = pars.to_dict()
-        gsf = CreateGSF(pars=pars.to_dict())
+        pars_renamed1 = rename_nld_columns(pars)
+
+        nld.pars = pars_renamed1.to_dict()
+        pars_renamed2 = rename_gsf_columns(pars)
+        gsf = CreateGSF(pars=pars_renamed2.to_dict())
 
         # nld = glede._nld
         # gsf = glede._gsf
@@ -507,7 +618,7 @@ def corner_plot(results: pd.DataFrame, n_samples: int = 10000,
     """
     from corner import corner
     if exclude is None:
-        exclude = ["p16", "p17", "p18", "p19",
+        exclude = ["gsf_p16", "gsf_p17", "gsf_p18", "gsf_p19",
                    'posterior_weights', 'LogLike', 'LogLike_isvalid']
     results_equal = results.sample(n=n_samples, weights="posterior_weights",
                                    random_state=6548)
@@ -521,6 +632,36 @@ def corner_plot(results: pd.DataFrame, n_samples: int = 10000,
     kwargs.setdefault("max_n_ticks", 4)
     fig = corner(results_equal, labels=results_equal.columns, **kwargs)
     return fig
+
+
+def rename_gsf_columns(df: Union[pd.DataFrame, pd.Series],
+                       copy=False, inplace=False,
+                       **kwargs) -> pd.DataFrame:
+    if isinstance(df, pd.DataFrame):
+        current_names = df.columns
+        axis = "columns"
+    elif isinstance(df, pd.Series):
+        current_names = df.index
+        axis = "index"
+    mapper = {col: col[4:] for col in current_names if col[:4] == 'gsf_'}
+    renamed = df.rename(mapper, axis=axis, copy=copy, inplace=inplace,
+                        **kwargs)
+    return renamed
+
+
+def rename_nld_columns(df: Union[pd.DataFrame, pd.Series],
+                       copy=False, inplace=False,
+                       **kwargs) -> pd.DataFrame:
+    if isinstance(df, pd.DataFrame):
+        current_names = df.columns
+        axis = "columns"
+    elif isinstance(df, pd.Series):
+        current_names = df.index
+        axis = "index"
+    mapper = {col: col[4:] for col in current_names if col[:4] == 'nld_'}
+    renamed = df.rename(mapper, axis=axis, copy=copy, inplace=inplace,
+                        **kwargs)
+    return renamed
 
 
 if __name__ == "__main__":
@@ -546,7 +687,8 @@ if __name__ == "__main__":
     glede._nld.model = hdfloader.nld_model
     print("finished")
 
-    results["B(M1)"] = results.apply(bm1, axis=1)
+    results_gsf = rename_gsf_columns(results)
+    results["B(M1)"] = results_gsf.apply(bm1, axis=1)
     print("calculated BM1")
 
     # create equally weighted samples
@@ -590,6 +732,10 @@ if __name__ == "__main__":
         pp.compare_firstgen(results.iloc[
                                 results['posterior_weights'].idxmax()])
     fig.savefig(figdir/"fg_posterior_max")
+    fig, ax = \
+        pp.compare_firstgen(results.iloc[
+                                results['LogLike'].idxmax()])
+    fig.savefig(figdir/"fg_loglike_max")
 
     for i, (_, row) in enumerate(results_equal.iterrows()):
         fig, ax = \
@@ -605,21 +751,27 @@ if __name__ == "__main__":
         if key in names["results"]:
             continue
         elif key in names["nld"]:
-            base = "nld_"
+            base = ""
         elif key in names["gsf"]:
-            base = "gsf_"
+            base = ""
         else:
             base = "dependent_"
         fig, _ = pp.plot_posterior_marginals(results, key, alpha=0.5)
         fig.savefig(figdir / (f'{base}{key}_posterior_hist.png'))
         plt.close(fig)
 
-    for key in ["D0_model", "Gg_model"]:
+    for key in ["D0_model", "Gg_model", "B(M1)"]:
         base = "dependent_"
-        histrange = [0, 10] if key == "D0_model" else [0, 500]
+        if key == "D0_model":
+            histrange = [0, 10]
+        elif key == "Gg_model":
+            histrange = [0, 500]
+        else:
+            histrange = [0, 50]
         fig, _ = pp.plot_posterior_marginals(results, key, alpha=0.5,
                                              histrange=histrange)
         fig.savefig(figdir / (f'{base}{key}_posterior_hist_zzom.png'))
+        plt.show()
         plt.close(fig)
 
     print("prepare corner plot")
